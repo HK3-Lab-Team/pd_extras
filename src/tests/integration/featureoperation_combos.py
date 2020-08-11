5c9bf01ffe884aadf9e31e022f0e3f5f4f1d362f
Lorenzo Gorini <lore.gorini@gmail.com>
2020-08-11 17:14:41 +0200

    Renamed prepare_featureoperation_combos into eq_featureoperation_fixture Added docstring to eq_featureoperation_fixture Replaced nested for loops with itertools.product

--- src/tests/integration/featureoperation_combos.py
+++ src/tests/integration/featureoperation_combos.py
@@ -1,4 +1,5 @@
 import collections
+import itertools
 
 from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
 
@@ -6,9 +7,29 @@ from ...pd_extras.dataframe_with_info import FeatureOperation
 from ...pd_extras.feature_enum import OperationTypeEnum
 
 
-def prepare_featureoperation_combos():
+def eq_featureoperation_fixture():
     """
-    Create different pairs of FeatureOperation instances to be compared for tests.
+    Create pairs of FeatureOperation instances to be compared for tests.
+
+    Many different pairs of FeatureOperation instances are created in order to test
+    the correct comparison between equal and unequal pairs.
+    When comparing FeatureOperation instances, ``__eq__`` method checks the attributes
+    related to:
+     1. operation type
+     2. original_columns (only if they are both not None)
+     3. derived_columns (only if they are both not None)
+     4. encoder (only if they are both not None)
+    Based on this, the method creates the possible pairs (combining same, different
+    and None values for each attribute)
+
+    Returns
+    -------
+    List[Tuple[Dict, Dict, boolean]]
+        List of tuples, representing the different FeatureOperation pairs to be tested.
+        Therefore each tuple contains: one dict for the attributes of the first
+        FeatureOperation instance, a second dict for the attributes of the second
+        FeatureOperation instance, and a boolean indicating if the two resulting
+        instances are actually equal or not.
     """
     attribute_possible_values = {
         "original_columns": [
@@ -42,45 +63,48 @@ def prepare_featureoperation_combos():
         (operation_types[0], operation_types[1], False),
         (operation_types[0], operation_types[0], True),
     ]
-    # Combine every possible pair of values for each attribute
+    # Combine every possible pair of values for each attribute and create a list with
+    # these pairs
     feat_operations_pairs_with_labels = []
-    for op_type_1, op_type_2, is_op_type_equal in operation_type_pairs_with_labels:
-        for (
-            orig_column_1,
-            orig_column_2,
-            is_orig_column_equal,
-        ) in pairs_per_attribute_with_label["original_columns"]:
-            for (
-                derived_column_1,
-                derived_column_2,
-                is_derived_column_equal,
-            ) in pairs_per_attribute_with_label["derived_columns"]:
-                for (
-                    encoder_1,
-                    encoder_2,
-                    is_encoder_equal,
-                ) in pairs_per_attribute_with_label["encoder"]:
-                    is_equal_label = (
-                        is_op_type_equal
-                        and is_orig_column_equal
-                        and is_derived_column_equal
-                        and is_encoder_equal
-                    )
-                    feat_operations_pairs_with_labels.append(
-                        (
-                            dict(
-                                operation_type=op_type_1,
-                                original_columns=orig_column_1,
-                                derived_columns=derived_column_1,
-                                encoder=encoder_1,
-                            ),
-                            dict(
-                                operation_type=op_type_2,
-                                original_columns=orig_column_2,
-                                derived_columns=derived_column_2,
-                                encoder=encoder_2,
-                            ),
-                            is_equal_label,
-                        )
-                    )
+    for (
+        op_type_tuple,
+        orig_column_tuple,
+        deriv_column_tuple,
+        encoder_tuple,
+    ) in itertools.product(
+        operation_type_pairs_with_labels,
+        pairs_per_attribute_with_label["original_columns"],
+        pairs_per_attribute_with_label["derived_columns"],
+        pairs_per_attribute_with_label["encoder"],
+    ):
+        # Unpack tuples from itertools
+        op_type_1, op_type_2, is_op_type_equal = op_type_tuple
+        orig_column_1, orig_column_2, is_orig_column_equal, = orig_column_tuple
+        deriv_column_1, deriv_column_2, is_deriv_column_equal, = deriv_column_tuple
+        encoder_1, encoder_2, is_encoder_equal = encoder_tuple
+
+        is_equal_label = (
+            is_op_type_equal
+            and is_orig_column_equal
+            and is_deriv_column_equal
+            and is_encoder_equal
+        )
+        feat_operations_pairs_with_labels.append(
+            (
+                dict(
+                    operation_type=op_type_1,
+                    original_columns=orig_column_1,
+                    derived_columns=deriv_column_1,
+                    encoder=encoder_1,
+                ),
+                dict(
+                    operation_type=op_type_2,
+                    original_columns=orig_column_2,
+                    derived_columns=deriv_column_2,
+                    encoder=encoder_2,
+                ),
+                is_equal_label,
+            )
+        )
+
     return feat_operations_pairs_with_labels
