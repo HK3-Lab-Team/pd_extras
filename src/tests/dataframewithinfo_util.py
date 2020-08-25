b9896838a97f464af5e1a99a31b86c358e83b252
Lorenzo Gorini <lore.gorini@gmail.com>
2020-08-25 16:32:14 +0200

    Completed tests for import/export_df_with_info functions for DataFrameWithInfo instances. Added two fixtures for creating generic files with 'shelve' module and the builtin functions. Fixed tests after rebase.

--- src/tests/dataframewithinfo_util.py
+++ src/tests/dataframewithinfo_util.py
@@ -4,11 +4,6 @@ from datetime import date
 from typing import Tuple
 
 import pandas as pd
-import pytest
-import sklearn
-
-from ..pd_extras.dataframe_with_info import DataFrameWithInfo, FeatureOperation
-from ..pd_extras.feature_enum import OperationTypeEnum
 
 
 class DataFrameMock:
