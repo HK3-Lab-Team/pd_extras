05a8c0fc0e901daf445f6dd5a7745971fe1338d7
alessiamarcolini <98marcolini@gmail.com>
2020-09-24 16:14:24 +0200

    Add util to convert any iterable to list

--- src/trousse/util.py
+++ src/trousse/util.py
@@ -1,6 +1,26 @@
 import functools
-from typing import Any, Callable
+from typing import Any, Callable, Iterable, List
 
 
 def lazy_property(f: Callable[..., Any]):
     return property(functools.lru_cache()(f))
+
+
+def tolist(iterable: Iterable) -> List[Any]:
+    """Convert any iterable to a list. A string is converted to a single element list.
+
+    Parameters
+    ----------
+    iterable : Iterable[Any]
+        The object to convert
+
+    Returns
+    -------
+    List[Any]
+        List after conversion
+    """
+    if isinstance(iterable, str):
+        iterable = [iterable]
+    elif not isinstance(iterable, list):
+        iterable = list(iterable)
+    return iterable
