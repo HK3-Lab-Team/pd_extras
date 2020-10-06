ee2cadc438ef54a3b4d5d4f97e561cc4d74bee46
Lorenzo Gorini <lore.gorini@gmail.com>
2020-10-06 02:28:59 +0200

    Modified the pipeline to simulate raw data in order to make it more similar to the actual experimental data and to make it compatible with the preprocessing tools of pytrousse. Added few functions to create some lists with data containing multiple type values. Added boolean column.

--- tests/preprocessing_util.py
+++ tests/preprocessing_util.py
@@ -20,16 +20,16 @@ from .datasim_util import (
 )
 
 WHOLE_WORD_REPLACE_DICT = {
-    "---": np.nan,
-    ".": np.nan,
-    "ASSENTI": np.nan,
-    "PRESENTI": np.nan,
-    "non disponibile": np.nan,
-    "NV": np.nan,
-    "-": np.nan,
-    "Error": np.nan,
-    "None": np.nan,
-    "NAN": np.nan
+    "---": pd.NA,
+    ".": pd.NA,
+    "ASSENTI": pd.NA,
+    "PRESENTI": pd.NA,
+    "non disponibile": pd.NA,
+    "NV": pd.NA,
+    "-": pd.NA,
+    "Error": pd.NA,
+    "None": pd.NA,
+    "NAN": pd.NA
     #     '0%': '0'
 }
 CHAR_REPLACE_DICT = {"°": "", ",": "."}
@@ -45,6 +45,58 @@ def _create_datetime_formatted_list(sample_size: int, datetime_format: str):
         date_list.append(date_element)
 
     return date_list
+
+
+def _int_data_list(sample_size: int, step: int = 1, bias: int = 0):
+    return [step * i + bias for i in range(sample_size)]
+
+
+def _float_data_list(sample_size: int, step: float, bias: float = 0):
+    return [step * i + bias for i in range(sample_size)]
+
+
+def _string_data_list(sample_size: int, prefix: str = "value"):
+    return [f"{prefix}_{i}" for i in range(sample_size)]
+
+
+def _categ_data_list(sample_size: int, categories_count: int, prefix: str = ""):
+    """
+    Create list of categorical data
+
+    The function creates a list with ``sample_size`` elements that are equally split
+    into ``categories_count`` subgroups that have the same repeated value (i.e.
+    the category).
+
+    Parameters
+    ----------
+    sample_size : int
+        Number of elements in the resulting list
+    categories_count : int
+        Number of different categories contained in the resulting list
+    prefix : str, optional
+        String that will be the prefix for each category. If set to "", the
+        categorical values will be integers. Default set to "".
+
+    Returns
+    -------
+    List
+        List of ``sample_size`` categorical samples.
+    """
+    samples_per_category = sample_size // categories_count
+    data_list = []
+    for cat in range(categories_count - 1):
+        data_list.extend(
+            [cat if prefix == "" else f"{prefix}{cat}"] * samples_per_category
+        )
+    data_list.extend(
+        [(categories_count - 1) if prefix == "" else f"{prefix}{categories_count-1}"]
+        * (sample_size - (categories_count - 1) * samples_per_category)
+    )
+    return data_list
+
+
+def _bool_data_list(sample_size: int, true_sample_count: int):
+    return [True] * true_sample_count + [False] * (sample_size - true_sample_count)
 
 
 def _create_ideal_test_dataset(sample_size: int) -> TestDataSet:
@@ -82,11 +134,11 @@ def _create_ideal_test_dataset(sample_size: int) -> TestDataSet:
 
     column_tuples = [
         # Metadata columns
-        ("metadata_int_col_0", list(range(sample_size))),
-        ("metadata_int_col_1", [i * 10 for i in range(sample_size)]),
-        ("metadata_float_col_0", [i * 0.0001 for i in list(range(sample_size))]),
-        ("metadata_str_col_0", [f"value0_{i}" for i in range(sample_size)]),
-        ("metadata_str_col_1", [f"value1_{i}" for i in range(sample_size)]),
+        ("metadata_int_col_0", _int_data_list(sample_size)),
+        ("metadata_int_col_1", _int_data_list(sample_size, step=10)),
+        ("metadata_float_col_0", _float_data_list(sample_size, step=0.0001)),
+        ("metadata_str_col_0", _string_data_list(sample_size, prefix="meta1")),
+        ("metadata_str_col_1", _string_data_list(sample_size, prefix="meta2")),
         (
             "metadata_str_categ_col",
             ["value0"] * one_half_samples
@@ -105,17 +157,18 @@ def _create_ideal_test_dataset(sample_size: int) -> TestDataSet:
         ("metadata_samestr_col", ["samevalue"] * sample_size),
         (
             "metadata_float_str_mixed_col",
-            [f"value0_{i}" for i in range(one_half_samples)]
-            + [23.1] * (sample_size - one_half_samples),
+            _string_data_list(one_half_samples)
+            + _float_data_list(sample_size - one_half_samples, step=0.5),
         ),
         (
             "metadata_int_float_mixed_col",
-            [2] * one_half_samples + [23.1] * (sample_size - one_half_samples),
+            _int_data_list(one_half_samples, step=2)
+            + _float_data_list(sample_size - one_half_samples, step=23.1),
         ),
         (
             "metadata_int_str_mixed_col",
-            [f"value0_{i}" for i in range(one_half_samples)]
-            + [23] * (sample_size - one_half_samples),
+            _string_data_list(one_half_samples)
+            + _int_data_list(one_half_samples, step=2),
         ),
         (
             "metadata_datetime_col",
@@ -150,34 +203,38 @@ def _create_ideal_test_dataset(sample_size: int) -> TestDataSet:
                 dtype=np.float32,
             ),
         ),
+        (
+            "feature_bool_col",
+            _bool_data_list(sample_size, one_half_samples),
+        ),
+        # Column with numerical values and explicit dtype="object"
+        ("feature_forcedstr_num_col", _float_data_list(sample_size, step=1.3)),
         ("feature_str_col_0", [f"value0_{i}" for i in range(sample_size)]),
         ("feature_str_col_1", [f"value1_{i}" for i in range(sample_size)]),
         (
             "feature_str_categ_col",
-            ["value0"] * one_half_samples
-            + ["value1"] * (sample_size - one_half_samples),
+            _categ_data_list(sample_size, categories_count=2, prefix="categ_"),
         ),
         (
             "feature_int_categ_col",
-            [1] * one_third_samples
-            + [2] * one_third_samples
-            + [3] * (sample_size - 2 * one_third_samples),
+            _categ_data_list(sample_size, categories_count=3, prefix=""),
         ),
-        ("feature_samenum_col", [0] * sample_size),
+        ("feature_samenum_col", [3] * sample_size),
         ("feature_samestr_col", ["samevalue"] * sample_size),
         (
             "feature_float_str_mixed_col",
-            [f"value0_{i}" for i in range(one_half_samples)]
-            + [23.1] * (sample_size - one_half_samples),
+            _string_data_list(one_half_samples)
+            + _float_data_list(sample_size - one_half_samples, step=0.5),
         ),
         (
             "feature_int_float_mixed_col",
-            [2] * one_half_samples + [23.1] * (sample_size - one_half_samples),
+            _int_data_list(one_half_samples, step=2)
+            + _float_data_list(sample_size - one_half_samples, step=23.1),
         ),
         (
             "feature_int_str_mixed_col",
-            [f"value0_{i}" for i in range(one_half_samples)]
-            + [23] * (sample_size - one_half_samples),
+            _string_data_list(one_half_samples)
+            + _int_data_list(one_half_samples, step=2),
         ),
         (
             "feature_datetime_col",
@@ -235,24 +292,7 @@ class CSVMock:
         """
         test_dataset = _create_ideal_test_dataset(sample_size=sample_size)
 
-        column_list = [
-            "metadata_str_categ_col",
-            "metadata_num_categ_col",
-            "metadata_float_str_mixed_col",
-            "metadata_int_float_mixed_col",
-            "metadata_int_str_mixed_col",
-            "metadata_datetime_col",
-            "metadata_date_col",
-            "metadata_onlyyear_col",
-            "metadata_int_col_0",
-            "metadata_int_col_1",
-            "metadata_float_col_0",
-            "metadata_str_col_0",
-            "metadata_str_col_1",
-            "metadata_nan_col",
-            "metadata_mostly_nan_col",
-            "metadata_samenum_col",
-            "metadata_samestr_col",
+        feature_columns = [
             "feature_bigfloat_col_0",
             "feature_bigfloat_col_1",
             "feature_str_categ_col",
@@ -271,54 +311,93 @@ class CSVMock:
             "feature_str_col_1",
             "feature_samenum_col",
             "feature_samestr_col",
+            "feature_bool_col",
         ]
-        only_string_columns = [
+        metadata_columns = [
             "metadata_str_categ_col",
+            "metadata_num_categ_col",
+            "metadata_float_str_mixed_col",
+            "metadata_int_float_mixed_col",
+            "metadata_int_str_mixed_col",
+            "metadata_datetime_col",
+            "metadata_date_col",
+            "metadata_onlyyear_col",
+            "metadata_int_col_0",
+            "metadata_int_col_1",
+            "metadata_float_col_0",
             "metadata_str_col_0",
             "metadata_str_col_1",
+            "metadata_nan_col",
+            "metadata_mostly_nan_col",
+            "metadata_samenum_col",
             "metadata_samestr_col",
+        ]
+        only_string_features = [
             "feature_str_categ_col",
             "feature_str_col_0",
             "feature_str_col_1",
             "feature_samestr_col",
         ]
-        float_columns = [
-            "metadata_float_str_mixed_col",
-            "metadata_int_float_mixed_col",
-            "metadata_float_col_0",
+        int_only_features = [
+            "feature_int_col_0",
+            "feature_int_col_1",
+            "feature_samenum_col",
+            "feature_int_categ_col",
+        ]
+        float_only_features = [
             "feature_bigfloat_col_0",
             "feature_bigfloat_col_1",
-            "feature_float_str_mixed_col",
-            "feature_int_float_mixed_col",
             "feature_smallfloat_col_0",
             "feature_smallfloat_col_1",
+            "feature_forcedstr_num_col",
         ]
-        datetime_columns = [
+        datetime_features = [
             "feature_datetime_col",
             "feature_date_col",
         ]
+        numerical_features = tuple(
+            set(int_only_features) | set(float_only_features) | {"feature_bool_col"}
+        )
 
         insert_nan_str_substr = Compose(
             [
                 ChangeColumnDType(
-                    column_names=["feature_bigfloat_col_0", "feature_smallfloat_col_0"],
+                    # This must be before the conversion to "Int32" for a known
+                    # pandas issue (https://github.com/pandas-dev/pandas/issues/25472)
+                    column_names=float_only_features + int_only_features,
                     new_dtype="object",
-                    dtype_after_fix="float",
+                    dtype_after_fix=np.float64,
                 ),
-                InsertNaNs(column_names=column_list, error_count=wrong_values_count),
+                ChangeColumnDType(
+                    column_names=int_only_features,
+                    new_dtype="object",
+                    dtype_after_fix=np.int64,
+                ),
+                ChangeColumnDType(
+                    column_names={"feature_bool_col"},
+                    new_dtype="object",
+                    dtype_after_fix=np.bool,
+                ),
+                InsertNaNs(
+                    column_names=metadata_columns + feature_columns,
+                    error_count=wrong_values_count,
+                    nan_value_to_insert=pd.NA,
+                    nan_value_after_fix=pd.NA,
+                ),
                 InsertNewValues(
-                    column_names=tuple(set(column_list) - set(only_string_columns)),
+                    # Numerical_features become mixed-type because of this operation
+                    column_names=numerical_features,
                     error_count=wrong_values_count,
                     replacement_map=WHOLE_WORD_REPLACE_DICT,
                 ),
-                InsertOutOfScaleValues(
-                    column_names=float_columns,
+                InsertNewValues(
+                    # String type feature do not get corrections
+                    column_names=only_string_features,
                     error_count=wrong_values_count,
-                    upperbound_increase=0.15,
-                    lowerbound_increase=0.15,
+                    replacement_map={"Error": "Error", "---": "---"},
                 ),
                 ReplaceSubstringsByValue(
-                    column_names=float_columns,
+                    column_names=float_only_features,
                     error_count=wrong_values_count,
                     replacement_map_list=[
                         # Replacing '.' with ','
@@ -326,24 +405,32 @@ class CSVMock:
                     ],
                 ),
                 InsertSubstringsByIndex(
-                    column_names=float_columns,
+                    column_names=numerical_features,
                     error_count=wrong_values_count,
                     replacement_map_list=[
+                        # inserting '°' symbol into a value, that will be corrected
+                        # by removing the symbol (leaving numerical value)
                         SubstringReplaceMapByIndex(-1, "°", "", True),
                         # inserting '%' symbol into a value, that will be corrected
                         # by changing the whole value to NaN
-                        SubstringReplaceMapByIndex(-1, "%", np.nan, False),
+                        SubstringReplaceMapByIndex(-1, "%", pd.NA, False),
                     ],
                 ),
                 # Introducing wrong datetime format (no correction is provided at
                 # the moment by PyTrousse, so the expectation is equal to the raw data)
                 ReplaceSubstringsByValue(
-                    column_names=datetime_columns,
+                    column_names=datetime_features,
                     error_count=wrong_values_count,
                     replacement_map_list=[
                         SubstringReplaceMapByValue("-", "/", "/", True),
                         SubstringReplaceMapByValue(":", ".", ".", True),
                     ],
+                ),
+                InsertOutOfScaleValues(
+                    column_names=numerical_features,
+                    error_count=wrong_values_count,
+                    upperbound_increase=0.02,
+                    lowerbound_increase=0.02,
                 ),
             ]
         )
